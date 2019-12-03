030d1cdff590176ce906dc019284a15786ba7f84
Nicolas Fonseca <nicolas.fonseca501@gmail.com>
2019-12-03 18:27:10 -0300

    add cloumn-max-width mixin

--- src/page/HomePage/HomePage.js
+++ src/page/HomePage/HomePage.js
@@ -2,17 +2,4 @@
 
 export default {
   name: 'HomePage',
-  data() {
-    return {
-      test: 1,
-    };
-  },
-  mounted() {
-    setTimeout(() => {
-      this.test = 2;
-      setTimeout(() => {
-        this.test = 1;
-      }, 2500);
-    }, 5000);
-  },
 };
